655b90c0425b6ecc665593e3a493044618d84243
aman-agar <aman.agar85@gmail.com>
2022-06-25 23:31:27 +0530

    Small fixes

--- segmentation_tool.py
+++ segmentation_tool.py
@@ -4,14 +4,20 @@ import os,cv2
 from keras.models import load_model
 import numpy as np
 
+file_path = ''
 model=load_model('D:\Python Programming\Flask\static\cat_vs_dog.h5')
 
 def prediction(file_path):
+    results=['CAT','DOG']
     img=cv2.imread(file_path)
     img=cv2.resize(img, (224,224),interpolation = cv2.INTER_NEAREST)
     img=np.expand_dims(img, axis=0)
     result=model.predict(img)
-    return result
+    if result<=0.5:
+        return results[0]
+    else:
+        return results[1]
+
 
 # FLASK APPLICATION
 app=Flask(__name__)
@@ -30,12 +36,9 @@ def upload():
       f = request.files['file']
       file_path=os.path.join(r'D:\Python Programming\Flask\static',secure_filename(f.filename))
       f.save(file_path)
+      #Call prediction to predict the output
       result=prediction(file_path)
-      if result<=0.5:
-        return 'CAT'
-      else:
-        return 'DOG0'
-
+      return result
 
 
 
